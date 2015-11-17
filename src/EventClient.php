da542352e70073a7339efb799b9603860662ba50
David Collard <dgcollard@gmail.com>
2015-11-17 15:00:06 +0000

    Revert "Added a basic updateParticipant function to allow additional data (e.g. JustGiving details) to be added to a participant."
    This reverts commit 5c9efa6c24504e736691c51b68a0945c94c17ade.

--- src/EventClient.php
+++ src/EventClient.php
@@ -172,15 +172,4 @@ class EventClient
             ]
         );
     }
-    
-    /**
-     * Update the participant, adding extra details
-     *
-     * This is useful for adding JustGiving details or payment status
-     */
-    public function updateParticipant($eventCode, $registrationId, $urn, $updateData)
-    {
-        return $this->http->patch(self::$baseUrl . '/events/' . $eventCode . '/registrations/' .
-            $registrationId . '/participant/' . $urn, $updateData);
-    }
 }
