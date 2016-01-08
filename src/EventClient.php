5a2c016456d1c3a97bfffefd9f105942949ba1a1
David Collard <dgcollard@gmail.com>
2016-01-08 14:03:45 +0000

    Add unit tests to cover use of $numTickets parameter

--- src/EventClient.php
+++ src/EventClient.php
@@ -170,7 +170,7 @@ class EventClient
      *
      * @param string $eventCode
      *   Event code
-     * @param integer $num_tickets
+     * @param integer $numTickets
      *   Number of tickets required (must be an integer between 1 and 10).
      * @return array
      *   Response body containing the new registration
@@ -180,7 +180,7 @@ class EventClient
         $uri = $this->path . "/events/$eventCode/registrations.json";
 
         return $this->requestJson('POST', $uri, [
-            'json' => ['tickets' => $numTickets]
+            'json' => ['tickets' => $numTickets],
         ]);
     }
 
