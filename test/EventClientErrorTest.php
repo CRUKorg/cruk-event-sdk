4787189536dd600c25c0880f139f36fae39a6379
David Collard <dgcollard@gmail.com>
2016-01-21 10:24:55 +0000

    Refactoad

--- test/EventClientErrorTest.php
+++ test/EventClientErrorTest.php
@@ -4,31 +4,13 @@ namespace Cruk\EventSdk\Test;
 
 use Cruk\EventSdk\EventClientError;
 
-class EventClientErrorTest extends \PHPUnit_Framework_TestCase
+class EventClientErrorTest extends TestCase
 {
-
-    /**
-     * EventClientError class
-     */
-    private $ewse;
-
-    public function setUp()
-    {
-        $this->ewse = new EventClientError();
-        parent::setUp();
-    }
-
     public function testDataCanBeRetrieved()
     {
-        $this->assertSame(array(), $this->ewse->getData());
-    }
+        $data = ['foo' => 'bar'];
+        $error = new EventClientError('', 0, null, $data);
 
-    public function testDataCanBeSet()
-    {
-        $data = array(
-            'error' => 'Awesome',
-        );
-        $this->ewse->setData($data);
-        $this->assertSame($data, $this->ewse->getData());
+        $this->assertSame($data, $error->getData());
     }
 }
