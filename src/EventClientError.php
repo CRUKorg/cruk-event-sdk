13e297d8f5cb7b42838397a7284d79e59e6520ad
David Collard <dgcollard@gmail.com>
2016-01-21 10:24:55 +0000

    Refactoad

--- src/EventClientError.php
+++ src/EventClientError.php
@@ -4,12 +4,11 @@ namespace Cruk\EventSdk;
 
 class EventClientError extends \Exception
 {
-
     /**
      * @var array $data
      *   Data array as returned by the EWS. It may be useful for us to be able to get a hold of this.
      */
-    protected $data;
+    private $data;
 
     /**
      * @return array
@@ -20,28 +19,21 @@ class EventClientError extends \Exception
     }
 
     /**
-     * @param array $data
-     */
-    public function setData($data)
-    {
-        $this->data = $data;
-    }
-
-    /**
      * Override the default constructor so that we can send through the data array sent from EWS.
      *
      * @param string $message
      *   The message that is displayed to the users
      * @param int $code
      *   Code
-     * @param Exception $previous
+     * @param \Exception $previous
      *   Previous exception
      * @param array $data
      *   Data array as sent from EWS. This can be retrieved, or is used
      */
-    public function __construct($message = '', $code = 0, Exception $previous = null, array $data = array())
+    public function __construct($message = '', $code = 0, \Exception $previous = null, array $data = array())
     {
         $this->data = $data;
+
         parent::__construct($message, $code, $previous);
     }
 }
