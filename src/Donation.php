9ce08a931ca4d66dadd829f330f0cbba4447ae65
Simon Rycroft <simon.rycroft@cancer.org.uk>
2016-01-25 08:51:39 +0000

    Keep the order of the overridden constructor parameters (i.e. keep  second as defined by EWSObject).

--- src/Donation.php
+++ src/Donation.php
@@ -126,11 +126,11 @@ class Donation extends EWSObject
     /**
      * Donation constructor.
      * @param EWSClient $client
+     * @param $data
      * @param Event $event
      * @param Registration $registration
-     * @param $data
      */
-    public function __construct(EWSClient $client, Event $event, Registration $registration, $data)
+    public function __construct(EWSClient $client, $data, Event $event, Registration $registration)
     {
         $this->event = $event;
         $this->registration = $registration;
