04fa6a379e2723c83c270353c7ee405682036648
Simon Rycroft <simon.rycroft@cancer.org.uk>
2016-01-28 17:05:34 +0000

    100% code coverage on the tests. Still need to add some failing tests, but this is much better than it was.

--- test/EWSClientTest.php
+++ test/EWSClientTest.php
@@ -60,6 +60,9 @@ class EWSClientTest extends TestCase
         // Execute the API call
         EWSClient::requestAccessToken($this->httpClient, 'my_client_id', 'my_client_secret');
 
+        // Test the constructor fully
+        new EWSClient($this->httpClient, 'my_client_id', 'my_client_secret');
+
         // Get the request from the history
         $request = $this->history[0]['request'];
 
@@ -70,6 +73,94 @@ class EWSClientTest extends TestCase
         // Assert that the query parameters are correct
         $this->assertRequestQueryParameterSame('client_id', 'my_client_id', $request);
         $this->assertRequestQueryParameterSame('client_secret', 'my_client_secret', $request);
+
+        // Test getting and setting accessToken
+        $this->ews = new EWSClient($this->httpClient, self::ACCESS_TOKEN);
+        $access_token = 'token';
+        $this->ews->setAccessToken($access_token);
+        $this->assertSame($access_token, $this->ews->getAccessToken());
+
+        // Test getting and setting the path
+        $this->ews = new EWSClient($this->httpClient, self::ACCESS_TOKEN);
+        $path = 'path';
+        $this->ews->setPath($path);
+        $this->assertSame($path, $this->ews->getPath());
+    }
+
+    public function testGettingAndSettingClient()
+    {
+        $event = new Event($this->ews, []);
+        $this->assertEquals($this->ews, $event->getClient());
+
+        // New client
+        $new_client = new EWSClient($this->httpClient, 'my_client_id_2', 'my_client_secret_2');
+        $event->setClient($new_client);
+        $this->assertEquals($new_client, $event->getClient());
+    }
+
+    public function testCreateEvent()
+    {
+        // Create the Event.
+        $event = new Event($this->ews, [
+            'eventName' => 'Banana shower of heavenly parents',
+            'eventType' => 'HUH',
+            'eventDistance' => '1000m',
+            'eventDistanceUOM' => '1000UOM',
+            'ageFrom' => 0,
+            'ageTo' => 100,
+            'ageCriteria' => 'Positive',
+            'gender' => 'male or female',
+            'waves' => [],
+            'sponsorshipPageCreationDuration' => '1m',
+            'defaultSiebelRegistrationStatus' => '',
+        ]);
+        $event->create();
+        $this->assertEquals('Banana shower of heavenly parents', $event->getEventName());
+        $this->assertEquals('HUH', $event->getEventType());
+        $this->assertEquals('1000m', $event->getEventDistance());
+        $this->assertEquals('1000UOM', $event->getEventDistanceUOM());
+        $this->assertEquals(0, $event->getAgeFrom());
+        $this->assertEquals(100, $event->getAgeTo());
+        $this->assertEquals('Positive', $event->getAgeCriteria());
+        $this->assertEquals('male or female', $event->getGender());
+        $this->assertEquals([], $event->getWaves());
+        $this->assertEquals('1m', $event->getSponsorshipPageCreationDuration());
+        $this->assertEquals('', $event->getDefaultSiebelRegistrationStatus());
+
+        // Get the request from the history
+        $request = $this->history[0]['request'];
+
+        // Assert that the request is made to the correct endpoint
+        $this->assertRequestMethodSame('POST', $request);
+        $this->assertRequestUriPathSame('api/v2/events.json', $request);
+
+        // Test setting registrations (possibly code that is not required).
+        $registration = new Registration($this->ews, [], $event);
+        $event->setRegistrations([$registration]);
+        $this->assertEquals([$registration], $event->getRegistrations());
+    }
+
+
+    public function test404()
+    {
+        $this->setExpectedException(EWSClientError::class);
+        // Create httpClient
+        $this->httpClient = $this->getHttpClient($this->history, [new Response(404, [], json_encode([]))]);
+        // Create the client with responses.
+        $this->ews = new EWSClient($this->httpClient, self::ACCESS_TOKEN);
+        // Create the Event.
+        $event = new Event($this->ews, 'N15RLM');
+    }
+
+    public function test201()
+    {
+        $this->setExpectedException(EWSClientError::class);
+        // Create httpClient
+        $this->httpClient = $this->getHttpClient($this->history, [new Response(201, [], json_encode([]))]);
+        // Create the client with responses.
+        $this->ews = new EWSClient($this->httpClient, self::ACCESS_TOKEN);
+        // Create the Event.
+        $event = new Event($this->ews, 'N15RLM');
     }
 
     public function testGetEvent()
@@ -124,12 +215,22 @@ class EWSClientTest extends TestCase
         new Event($ews, 'MISSING');
     }
 
+    public function testInabilityToPatchRegistration()
+    {
+        $this->setExpectedException(EWSClientError::class);
+
+        $event = new Event($this->ews, []);
+        $registration = new Registration($this->ews, [], $event);
+        $registration->patch();
+    }
+
     public function testGetEventRegistration()
     {
         // Create the Event. (Response 0)
         $event = new Event($this->ews, 'N15RLM');
         // Get availability. (Response 1)
-        new Registration($this->ews, 123, $event);
+        $registration = new Registration($this->ews, 123, $event);
+        $registration->asArray();
 
         // Get the request from the history
         $request = $this->history[1]['request'];
@@ -208,7 +309,8 @@ class EWSClientTest extends TestCase
         // Get availability. (Response 1)
         $registration = new Registration($this->ews, 123, $event);
         // Get participant. (Response 2)
-        new Participant($this->ews, 'a1b2c3d4', $event, $registration);
+        $participant = new Participant($this->ews, 'a1b2c3d4', $event, $registration);
+        $participant->asArray();
 
         // Get the request from the history
         $request = $this->history[2]['request'];
@@ -228,9 +330,21 @@ class EWSClientTest extends TestCase
         // Get availability. (Response 1)
         $registration = new Registration($this->ews, 123, $event);
         // Create participant. (Response 2)
-        $registration->createParticipant([
+        $participant = $registration->createParticipant([
             'forename' => 'participant_value'
         ]);
+        $this->assertEquals([$participant], $registration->getParticipants());
+        $registration->setParticipants([$participant]);
+        $this->assertEquals([$participant], $registration->getParticipants());
+
+        // Set timeout
+        $registration->setTimeOut('1970-01-01 00:00 GMT');
+        $this->assertEquals('1970-01-01 00:00 GMT', $registration->getTimeOut());
+        $this->assertEquals(0, $registration->getUnixTimeOut());
+
+        // Test status
+        $registration->setStatus('complete');
+        $this->assertEquals('complete', $registration->getStatus());
 
         // Get the request from the history
         $request = $this->history[2]['request'];
@@ -302,9 +416,40 @@ class EWSClientTest extends TestCase
         // Get availability. (Response 1)
         $registration = new Registration($this->ews, 123, $event);
         // Create the Donation. (Response 2)
-        $registration->createDonation([
-            'bankAccountCode' => 'donation_value'
+        $donation = $registration->createDonation([
+            'id' => '100',
+            'bankAccountCode' => 'donation_value',
+            'amount' => 1000,
+            'dataSource' => 'data_source',
+            'donationType' => 'donation_type',
+            'financialPaymentReference' => 'financial_pay_ref',
+            'paymentMethod' => 'payment_method',
+            'paymentStatus' => 'payment_status',
+            'dateReceived' => '2000-01-01',
+            'personalGiftAid' => 'yep',
+            'product' => 'rabbits',
+            'source' => 'over these',
+            'originalPaymentId' => 'blahalksgh',
+            'toBeGiftAided' => 'nope',
+            'paymentProviderTransactionId' => '1234567890',
         ]);
+        $this->assertEquals('2000-01-01', $donation->getDateReceived());
+        $this->assertEquals(100, $donation->getId());
+        $this->assertEquals(1000, $donation->getAmount());
+        $this->assertEquals('data_source', $donation->getDataSource());
+        $this->assertEquals('donation_type', $donation->getDonationType());
+        $this->assertEquals('financial_pay_ref', $donation->getFinancialPaymentReference());
+        $this->assertEquals('payment_method', $donation->getPaymentMethod());
+        $this->assertEquals('payment_status', $donation->getPaymentStatus());
+        $this->assertEquals('2000-01-01', $donation->getDateReceived());
+        $this->assertEquals('yep', $donation->getPersonalGiftAid());
+        $this->assertEquals('rabbits', $donation->getProduct());
+        $this->assertEquals('over these', $donation->getSource());
+        $this->assertEquals('blahalksgh', $donation->getOriginalPaymentId());
+        $this->assertEquals('1234567890', $donation->getPaymentProviderTransactionId());
+
+        $this->assertEquals(100, $registration->getDonationId());
+        $this->assertEquals($donation, $registration->getDonation());
 
         // Get the request from the history
         $request = $this->history[2]['request'];
@@ -318,5 +463,100 @@ class EWSClientTest extends TestCase
 
         // Assert that the request uses the OAuth access token to authenticate
         $this->assertRequestAuthenticates(self::ACCESS_TOKEN, $request);
+    }
+
+    public function testCreateParticipantFromArray()
+    {
+        // Create the Event. (Response 0)
+        $event = new Event($this->ews, 'N15RLM');
+        // Get availability. (Response 1)
+        $registration = new Registration($this->ews, 123, $event);
+        // Create the participant
+        $data = [
+            'primaryParticipant' => true,
+            'participant' => [
+                'siebelId' => 'siebelid',
+                'forename' => 'Wow',
+                'surname' => 'McPants',
+                'title' => 'Mrs',
+                'gender' => 'male',
+                'dob' => '1969-01-01',
+                'primaryDataSourceCode' => 'yessir',
+                'address' => [
+                    'firstline' => 'First line'
+                ],
+                'emailAddress' => 'email@email.com',
+                'telephoneNumberLandline' => '02012345678',
+                'telephoneNumberMobile' => '07777777777',
+                'suppressions' => [
+                    'SUP'
+                ],
+            ],
+            'eventSpecific' => [
+                'tshirtSizeCode' => 'abcdef',
+                'fundraisingUrl' => 'http://www.justgiving.com/gimme-your-money',
+                'fundraisingTarget' => 10000,
+                'emergencyContactName' => 'Smarty McPants',
+                'emergencyContactNumber' => '07111111111111',
+                'runningNumber' => 1000,
+                'cancerType' => 'head cancer',
+                'motivation' => 'Head hurts',
+            ],
+        ];
+        $participant = new Participant($this->ews, $data, $event, $registration);
+        $this->assertEquals($registration, $participant->getRegistration());
+        $participant->setRegistration($registration);
+        $this->assertEquals($registration, $participant->getRegistration());
+        $this->assertEquals($event, $participant->getEvent());
+        $participant->setEvent($event);
+        $this->assertEquals($event, $participant->getEvent());
+        $this->assertEquals($data, $participant->asArray());
+
+        // Check we can get a value from a key
+        $this->assertEquals('Smarty McPants', $participant->getValueFromKey('emergencyContactName'));
+
+        // Check that a missing key returns null
+        $this->assertEquals(null, $participant->getValueFromKey('trump'));
+    }
+
+    public function testUpdateParticipant()
+    {
+        $response_body = [
+            'registrationId' => 123,
+            'access_token' => 'here-be-my-token',
+        ];
+        $this->responses = [
+            new Response(200, [], json_encode($response_body)),
+            new Response(200, [], json_encode($response_body)),
+        ];
+        // History that's going to be populated by the HTTP Client.
+        $this->history = [];
+        // Create httpClient
+        $this->httpClient = $this->getHttpClient($this->history, $this->responses);
+        // Create the client with responses.
+        $this->ews = new EWSClient($this->httpClient, self::ACCESS_TOKEN);
+
+        // Create the Event.
+        $event = new Event($this->ews, []);
+        // Get availability.
+        $registration = new Registration($this->ews, [], $event);
+        // Create the participant
+        $participant = new Participant($this->ews, [], $event, $registration);
+
+        $participant->setForename('Howard');
+        $this->assertEquals('Howard', $participant->getForename());
+
+        // Response 0
+        $participant->patch();
+        // Response 1
+        $participant->update();
+
+        // Get request from history
+        $request = $this->history[0]['request'];
+        $this->assertRequestMethodSame('PATCH', $request);
+
+        // Get request from history
+        $request = $this->history[1]['request'];
+        $this->assertRequestMethodSame('PUT', $request);
     }
 }
